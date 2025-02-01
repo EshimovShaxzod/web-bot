65f6298922e90dd9139c62cffdb8e42cb2506652
EshimovShaxzod <eshimov474@gmail.com>
2025-02-01 18:42:55 +0500

    done telegram web bot

---
+++ src/constants/db.js
@@ -0,0 +1,60 @@
+export function getData() {
+  return [
+    {
+      title: "JavaScript",
+      price: 17.99,
+      Image:
+        "/images/javascript.png",
+      id: 1,
+    },
+    {
+      title: "ReactJS",
+      price: 15,
+      Image:
+        "/images/javascript.png",
+      id: 2,
+    },
+    {
+      title: "VueJS",
+      price: 13.5,
+      Image:
+        "/images/javascript.png",
+      id: 3,
+    },
+    {
+      title: "NodeJS",
+      price: 13.99,
+      Image:
+        "/images/javascript.png",
+      id: 4,
+    },
+    {
+      title: "NestJS",
+      price: 12.5,
+      Image:
+        "/images/javascript.png",
+      id: 5,
+    },
+    {
+      title: "NextJS",
+      price: 12.99,
+      Image:
+        "/images/javascript.png",
+      id: 6,
+    },
+    {
+      title: "TypeScript",
+      price: 22.99,
+      Image:
+        "/images/javascript.png",
+      id: 7,
+    },
+    {
+      title: "TensorflowJS",
+      price: 28.99,
+      Image:
+        "/images/javascript.png",
+      id: 8,
+    },
+  ];
+}
